23e7c6b61961fde19c4ca1c94098b1d9cf505626
bellukchips <bellukchips@gmail.com>
2025-06-23 19:54:15 +0700

    fix

--- src/app/components/Overview.tsx
+++ src/app/components/Overview.tsx
@@ -67,7 +67,8 @@ export default function Overview() {
 
                 {/* Button Save the Date */}
                 <div className="mb-12">
-                    <a href={calendarUrl} className="inline-flex items-center gap-2 px-6 py-3 bg-white/90 backdrop-blur-sm rounded-full shadow-lg text-sm font-medium font-mono text-gray-800 hover:bg-white hover:shadow-xl transition-all duration-300">
+                    <a href={calendarUrl} target="_blank"
+                        rel="noopener noreferrer" className="inline-flex items-center gap-2 px-6 py-3 bg-white/90 backdrop-blur-sm rounded-full shadow-lg text-sm font-medium font-mono text-gray-800 hover:bg-white hover:shadow-xl transition-all duration-300">
                         <svg className="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m11.5 11.5 2.071 1.994M4 10h5m11 0h-1.5M12 7V4M7 7V4m10 3V4m-7 13H8v-2l5.227-5.292a1.46 1.46 0 0 1 2.065 2.065L10 17Zm-5 3h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
                         </svg>
